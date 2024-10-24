822d1dde0722e79f9b7144d74a5cc60134ef50e7
Michael Lyga <31606955+mkwiiallpro@users.noreply.github.com>
2024-10-24 15:10:36 -0400

    results calculator
    TODO: Write a complete test suite for this

--- twow/parse_votes.js
+++ twow/parse_votes.js
@@ -7,15 +7,15 @@ function screen_maker(all_responses,size,seed){
     
     buffer = parseInt(Math.abs(seed))
     if(buffer=== NaN){
-        console.log("ERROR: Seed not an integer")
+        console.error("ERROR: Seed not an integer")
         return -1
     }
     if(size < 7){
-        console.log("ERROR: Section size too small")
+        console.error("ERROR: Section size too small")
         return -1
     }
     if(size > all_responses.length){
-        console.log("ERROR: Section size too big")
+        console.error("ERROR: Section size too big")
         return -1
     }
     const a = 25214903917
@@ -58,7 +58,8 @@ function print_section(section){
         console.log("--------------------")
     }
 }
-function check_vote(vote,size){
+function check_vote(all_responses,vote,size){
+    // The absolute basics
     if(vote.at(0) !== "[" || vote.at(-1) !== "]"){
         return "ERROR: Missing Bracket(s)"
     }
@@ -67,8 +68,25 @@ function check_vote(vote,size){
         return "ERROR: Wrong number of spaces"
     }
     let keyword =both_sides[0]
-    // TODO: Keyword Checking
 
+
+    // Keyword Checking
+    const section_data = both_sides[0].substring(1).split("-") // "[1-7-1" becomes [1,7,1], string and number implicit conversion
+    if(section_data.length !== 3){
+        return "Keyword Error: Bad Keyword"
+    }
+    let return_code = 0
+    try{
+        return_code = screen_maker(all_responses,section_data[1],section_data[0]) // responses,size,seed!
+        if(return_code.screens.length <= section_data[2]){
+            return "Keyword Error: Not enough screens in the section"
+        }
+    }
+    catch(err){
+        return "Keyword Error: "+err
+    }
+    
+    //  Recording good/bad letters according to size
     let letters = both_sides[1].substr(0,both_sides[1].length-1)
     const counts = []
     const bad = []
@@ -82,6 +100,7 @@ function check_vote(vote,size){
         }
         else counts[temp]++ 
     }
+
     // Letter Checking
     const missing = []
     const multiple = []
@@ -137,6 +156,7 @@ function random_vote(screen_name,size){
 
 let responses = []
 
+// File IO
 const fs = require("fs")
 const buf = Buffer.alloc(999999)
 const readline = require("readline")
@@ -161,12 +181,14 @@ print_section(screen_maker(responses,16,1111)) // Again, screen size isn't alway
 
 
 const results = new Map()
+
 for(let i =0; i<responses.length;i++){
     results.set(responses[i],[]) // All repsonses start with zero votes. Just doing this in advance so I don't need to do it later.
 }
 
-const votes = ["[420-69-3 BABABNAS]","[1111-11-1 ABCEFGH]","[BABABNAS THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG]"]
-for(let i =0; i<10; i++){
+const votes =  []
+//["[BABABNAS ABCDEFGH]","[593-8-2 ABCDEFGH]","[600-6-0 ABCDEFGH]"]
+for(let i =0; i<5; i++){
     votes.push(random_vote("592-8-0",8))
     votes.push(random_vote("592-8-1",8))
     votes.push(random_vote("16384-16-0",16))
@@ -174,10 +196,30 @@ for(let i =0; i<10; i++){
 for(let i =0; i<votes.length;i++){
     let vote = votes[i]
     console.log(vote)
-    console.log(check_vote(vote,8))
+    console.log(check_vote(responses,vote,8))
 }
 //console.log(votes)
 
 // TODO: Open another file and parse votes
 
-// Results Calculation
+// Calculate Results, assumes all votes are good
+// TODO: Error Checking, although bad votes will be rejected on arrival so the votes array should be 100% clean
+
+for(let i =0; i<votes.length; i++){
+    const both_sides = votes[i].split(" ") // "[1-7-1 ABCDEFG]" becomes ["[1-7-1" ,"ABCDEFG]"]
+    const section_data = both_sides[0].substring(1).split("-") // "[1-7-1" becomes [1,7,1], string and number implicit conversion
+    const letters = both_sides[1].substring(0,both_sides[1].length-1) // "ABCDEFG]" becomes "ABCDEFG"
+    const section_object = screen_maker(responses,section_data[1],section_data[0]) // no clue why they're reversed
+
+    const current_screen = section_object.screens[section_data[2]]
+    // DEBUG console.log(current_screen)
+    for(let j =0; j<current_screen.length;j++){
+        let temp = letters.charCodeAt(j)-65 // pay VERY close attention to loop letters
+        
+        let current_response = current_screen[temp]
+        
+        let percentile = (current_screen.length-1-j)/(current_screen.length-1)
+        results.get(current_response).push(percentile)
+    }
+}
+console.log(results)
